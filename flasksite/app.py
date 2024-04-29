4daf837002cd369a9e0bf25bacae360973d8fe86
latheef613 <167701467+latheef613@users.noreply.github.com>
2024-04-28 20:18:43 -0800

    Delete flasksite directory

--- flasksite/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-new=Flask(__name__)
-
-
-@new.route('/profile <int:id>')
-def profile(id):
-    return  '<h1>this profile is page %d </h1>' % username
-
-new.run(debug=True)
